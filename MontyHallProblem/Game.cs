56d3b1d2742fc56f936a05e13e852e13ca15c90e
Lal <zeta.amrithlal@gmail.com>
2024-11-15 20:58:52 +0530

    Initial changes

---
+++ MontyHallProblem/Game.cs
@@ -0,0 +1,59 @@
+﻿
+namespace MontyHallProblem
+{
+    public struct GameState
+    {
+        public int HiddenPrizePos { get; set; }
+        public int RevealedPos { get; set; }
+        public int FirstSelection { get; set; }
+        public int SwitchSelection { get; set; }
+
+        public int IterationCount { get; set; }
+        public int FirstSelectionWinCount { get; set; }
+        public int SwitchSelectionWinCount { get; set; }
+    }
+
+    public class Game
+    {
+        GameState state = new();
+        public GameState GetState() => state;
+        public void Reset() => state = new GameState();
+
+        private void ResetChoice()
+        {
+            state.HiddenPrizePos = 0;
+            state.RevealedPos = 0;
+            state.FirstSelection =
+            state.SwitchSelection = 0;
+        }
+
+        private void SelectPositions()
+        {
+            int[] positions = new int[3] { 1, 2, 3 };
+
+            state.HiddenPrizePos = positions[Random.Shared.Next(positions.Length)];
+            state.FirstSelection = positions[Random.Shared.Next(positions.Length)];
+
+            // Select random door to reveal. Exclude prize and selected position
+            int[] remainPos = positions.Where(x => x != state.HiddenPrizePos && x != state.FirstSelection).ToArray();
+            state.RevealedPos = remainPos[Random.Shared.Next(remainPos.Length)];
+
+            // Switch selection from first to unrevealed option
+            state.SwitchSelection = positions.SingleOrDefault(x => x != state.FirstSelection && x != state.RevealedPos);
+        }
+
+        private void Calculate()
+        {
+            state.IterationCount++;
+            if (state.HiddenPrizePos == state.FirstSelection) state.FirstSelectionWinCount++;
+            if (state.HiddenPrizePos == state.SwitchSelection) state.SwitchSelectionWinCount++;
+        }
+
+        public void Iterate()
+        {
+            ResetChoice();
+            SelectPositions();
+            Calculate();
+        }
+    }
+}
---
+++ MontyHallProblem/Program.cs
@@ -0,0 +1,22 @@
+﻿using MontyHallProblem;
+
+Console.WriteLine("Starting..");
+
+int iterationCount = 100;
+GameState state = new();
+Game game = new();
+for (int i = 0; i < iterationCount; i++)
+{
+    game.Iterate();
+    state = game.GetState();
+    Console.Write($"{state.IterationCount}, ");
+    Console.Write($"Prize: {state.HiddenPrizePos}, Selection: {state.FirstSelection}, Reveal: {state.RevealedPos}, Switch: {state.SwitchSelection}");
+    Console.WriteLine();
+}
+Console.WriteLine();
+Console.WriteLine($"Success rate without switch : {Math.Round((((double)state.FirstSelectionWinCount / state.IterationCount) * 100),2)}%");
+Console.WriteLine($"Success rate after switch   : {Math.Round((((double)state.SwitchSelectionWinCount / state.IterationCount) * 100),2)}%");
+Console.WriteLine();
+
+Console.WriteLine("Press enter to exit");
+Console.ReadLine();
